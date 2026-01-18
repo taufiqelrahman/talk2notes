0c79e7ff501d538285a6f2feec518464d35a6157
Taufiq <taufiqelrahman65@gmail.com>
2026-01-18 19:08:51 +0700

    feat: add bounce animation and keyframes to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,21 @@ module.exports = {
           900: '#0c4a6e',
         },
       },
+      animation: {
+        bounce: 'bounce 1s infinite',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
